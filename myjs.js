f36d748cca6f9f9b6172a959d244b22c75381123
Satyam Panchal <satyampanch@gmail.com>
2018-11-04 11:03:59 +0530

    Added My Website

---
+++ myjs.js
@@ -0,0 +1,86 @@
+/*Navigation Bar*/
+/*
+jQuery(function() {
+    var header = jQuery(".x-navbar");
+    jQuery(window).scroll(function() {
+        var scroll = jQuery(window).scrollTop();
+
+        if (scroll >= 50) {
+             header.removeClass('darknavbar').addClass("clearnavbar");
+        } else {
+             header.removeClass("clearnavbar").addClass('darknavbar');
+        }
+    });
+});
+jQuery(function() {
+    var header = jQuery(".x-navbar a");
+    jQuery(window).scroll(function() {
+        var scroll = jQuery(window).scrollTop();
+
+        if (scroll >= 50) {
+             header.removeClass('white').addClass("black");
+        } else {
+             header.removeClass("black");
+        }
+    });
+});
+*/
+function myFunction() {
+    var x = document.getElementById("nheader-r");
+    if (x.className === "header-r") {
+        x.className += " responsive";
+    } else {
+        x.className = "header-r";
+    }
+}
+
+
+
+/*End Navigation Bar*/
+jQuery(function() {
+    var header = jQuery(".skill_one");
+    jQuery(window).scroll(function() {
+        var scroll = jQuery(window).scrollTop();
+
+        if (scroll >= 100) {
+             $('.skill_one').animate({width:'100%'}, 1000);
+        }
+    });
+});
+
+jQuery(function() {
+    var header = jQuery(".dish-box");
+    jQuery(window).scroll(function() {
+        var scroll = jQuery(window).scrollTop();
+         if (scroll >= 370) {
+          $('.d3').fadeIn();
+        }
+         if (scroll>=670) {
+          $('.d4').fadeIn();
+        }
+        if (scroll>=970) {
+         $('.d5').fadeIn();
+       }
+       if (scroll>=1370) {
+        $('.d6').fadeIn();
+      }
+      if (scroll>=1670) {
+       $('.d7').fadeIn();
+     }
+     if (scroll>=1970) {
+      $('.d8').fadeIn();
+    }
+    if (scroll>=2270) {
+     $('.d9').fadeIn();
+   }
+   if (scroll>=2570) {
+    $('.d10').fadeIn();
+  }
+  if (scroll>=2870) {
+   $('.d11').fadeIn();
+ }
+ if (scroll>=3170) {
+  $('.d12').fadeIn();
+}
+    });
+});
