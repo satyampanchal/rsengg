b40245f02ec86fb1f7c6cbbcb9fdd353307c08f5
satyampanchal <satyampanch@gmail.com>
2018-11-16 17:03:02 +0530

    Added loader gif

--- myjs.js
+++ myjs.js
@@ -84,3 +84,15 @@ jQuery(function() {
 }
     });
 });
+
+
+
+(function($){
+  "use strict";
+
+	// Preloader Js
+	jQuery(window).on('load', function() {
+		jQuery("#status").fadeOut();
+		jQuery("#preloader").delay(200).fadeOut("slow");
+	});
+})();
