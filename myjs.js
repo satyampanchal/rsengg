8aa5299fe8e7b294182d4cf7b2f03730685dbe57
satyampanchal <satyampanch@gmail.com>
2018-11-16 17:34:16 +0530

    Added Loader again

--- myjs.js
+++ myjs.js
@@ -84,3 +84,13 @@ jQuery(function() {
 }
     });
 });
+
+(function($){
+  "use strict";
+
+	// Preloader Js
+	jQuery(window).on('load', function() {
+		jQuery("#status").fadeOut();
+		jQuery("#preloader").delay(200).fadeOut("slow");
+	});
+})();
