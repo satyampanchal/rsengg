d1434fd4472dfc3a0e077a65e9abbf9d524720ca
satyampanchal <satyampanch@gmail.com>
2018-11-20 12:09:19 +0530

    Fix location

--- myjs.js
+++ myjs.js
@@ -98,7 +98,7 @@ jQuery(function() {
 
 function initMap() {
   // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
+  var uluru = {lat: 21.089003, lng: 72.915675};
   // The map, centered at Uluru
   var map = new google.maps.Map(
       document.getElementById('map'), {zoom: 4, center: uluru});
