aab4a37030463882eae95b67ea41638bc7e4da2b
satyampanchal <satyampanch@gmail.com>
2018-11-20 12:23:49 +0530

    .

--- myjs.js
+++ myjs.js
@@ -94,14 +94,3 @@ jQuery(function() {
 		jQuery("#preloader").delay(200).fadeOut("slow");
 	});
 })();
-
-
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: 21.089003, lng: 72.915675};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
