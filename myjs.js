e7e9cc964223657b3a249ebe126ac6ff9716d8c6
satyampanchal <satyampanch@gmail.com>
2018-12-23 16:39:20 +0530

    .

--- myjs.js
+++ myjs.js
@@ -183,8 +183,6 @@ $('.clientslider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
-
-
       }
     }
     // You can unslick at a given breakpoint now by adding:
