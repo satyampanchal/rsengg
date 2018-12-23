49afe94602edb258494ee4538293d3d12cc0aa14
satyampanchal <satyampanch@gmail.com>
2018-12-23 16:21:26 +0530

    .

--- myjs.js
+++ myjs.js
@@ -151,3 +151,44 @@ $('.dishendslider').slick({
     // instead of a settings object
   ]
 });
+
+
+$('.clientslider').slick({
+  dots: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+
+
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
